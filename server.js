1d22da251d8167131851af63c353ae3cecdc33de
Alexander Vassbotn Røyne-Helgesen <alexander@phun-ky.net>
2014-01-06 10:49:58 +0100

    Reverted changes

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 
 var express = require('express');
 var request = require('request');
-var fs = require('fs');
 var app = express();
 
 
 var demo_url = "https://api.github.com/users/bekkopen/repos";
 app.get('/', function(req, res) {
-  request('http://upload.wikimedia.org/wikipedia/commons/thumb/6/62/Super_Series_2007_Scoreboard.jpg/800px-Super_Series_2007_Scoreboard.jpg').pipe(fs.createWriteStream('doodle.png'));
-
+  request.get({
+    url: demo_url,
+    json: true,
+    headers: {
+            'User-Agent': 'request'
+                }
+    }, function(error, response, body) {
+      if(error) {
+        console.log("an error has occured. keep calm and carry on.");
+      }
+      res.json(body);
+    });
 
 
 });
