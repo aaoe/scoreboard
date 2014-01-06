3fa4bbc950ebbc44dc4bba24a9f9b5ebe4edad66
Alexander Vassbotn Røyne-Helgesen <alexander@phun-ky.net>
2014-01-06 10:37:33 +0100

    testing images

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var app = express();
 
 var demo_url = "https://api.github.com/users/bekkopen/repos";
 app.get('/', function(req, res) {
-  request('https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRkjEP5RNqYGnSWr6H-yYr4-wMnXmZo1Gjt6BhpO7m49D4p0eJV').pipe(fs.createWriteStream('doodle.png'));
+  request('http://upload.wikimedia.org/wikipedia/commons/thumb/6/62/Super_Series_2007_Scoreboard.jpg/800px-Super_Series_2007_Scoreboard.jpg').pipe(fs.createWriteStream('doodle.png'));
 
 
 
