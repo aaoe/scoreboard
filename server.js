166eea881b6fd2601f73ede9403cdd36b3338be9
Øyvind Voldsund <oyvindvol@gmail.com>
2014-01-06 14:09:26 +0100

    statuskoder message med id

--- server.js
+++ server.js
@@ -37,13 +37,19 @@ app.get('/message/:id', function(req, res) {
     json: true,
     headers: headers
     }, function(error, response, body) {
-      if(error) {
-        console.log("an error has occured. keep calm and carry on.");
-      }
-      res.json(body);
+      respond(error, res, body);
     });
 });
 
+function respond(error, res, body) {
+  if(error) {
+    return res.send(body.statuscode, 'Oh no!');
+  } else if (body.statuscode == 500) {
+    return res.send(204);
+  } else {
+    return res.json(body);
+  }
+}
 
 // if on heroku use heroku port.
 var port = process.env.PORT || 1339;
