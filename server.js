992d6cb6e2cf3fb9d4729db3f42230fb7190be38
Øyvind Voldsund <oyvindvol@gmail.com>
2014-01-06 11:23:43 +0100

    auth

--- server.js
+++ server.js
@@ -8,9 +8,6 @@ var pass = process.env.BC_PASSWORD || 'pass';
 var api_url = "/api";
 var api_base_url = 'http://'+ user + ':' + pass + '@' + process.env.API_URL + api_url || "http://localhost" + api_url;
 
-
-app.use(express.basicAuth(user, pass));
-
 app.get('/messages', function(req, res) {
   request.get({
     url: api_base_url + '/messages',
