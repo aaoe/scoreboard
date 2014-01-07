6a7ade2114ce07fd82c45ff0304fd8356038504d
Anders Kristiansen <anderskri@gmail.com>
2014-01-07 09:12:22 +0100

    Lagt til integrasjon med ansattliste

---
+++ ansattListe.js
@@ -0,0 +1,63 @@
+var request = require('request');
+var q = require('q');
+var _ = require('underscore');
+
+var user = process.env.BC_USERNAME;
+var pass = process.env.BC_PASSWORD;
+var authentication = 'http://'+ user + ':' + pass + '@';
+var user_api_base_url =  authentication + process.env.USER_API_URL || "http://localhost";
+
+var headers = {
+  'User-Agent': 'request'
+};
+
+var ansatte = [];
+
+function cacheAnsattListe(){
+	
+	request.get({
+    url: user_api_base_url + '/all',
+    json: true,
+    headers: headers
+  }, function(error, response, body) {
+    if(error) {
+      console.log("an error has occured. keep calm and carry on.");
+    }
+
+    _.each(body, function(employee){
+    	return getAnsatt(employee.Id).then(function(fullEmployee) {
+    		console.log(fullEmployee[0].Seniority);
+    		ansatte.push(fullEmployee[0]);
+    	});
+    });
+
+  });
+}
+
+function getAnsatt(id) {
+	var deferred = q.defer();
+
+ request.get({
+    url: user_api_base_url + '/employee/' + id,
+    json: true,
+    headers: headers
+  }, function(error, response, body) {
+    if(error) {
+      console.log("an error has occured. keep calm and carry on.");
+      deferred.reject("Error!");
+    }
+    
+    deferred.resolve(body);
+  });
+ 
+ 	return deferred.promise;
+}
+
+function getAnsatte() {
+	return ansatte;
+}
+
+module.exports = {
+	cacheAnsattListe: cacheAnsattListe,
+	getAnsatte: getAnsatte
+}
--- server.js
+++ server.js
@@ -2,33 +2,58 @@
 var express = require('express');
 var request = require('request');
 var _ = require('underscore');
+var http = require('http');
+var q = require('q');
 var app = express();
+
+var ansattListe = require('./ansattListe.js');
 
 var user = process.env.BC_USERNAME;
 var pass = process.env.BC_PASSWORD;
 var api_url = "/api";
 var authentication = 'http://'+ user + ':' + pass + '@';
 var api_base_url =  authentication + process.env.API_URL + api_url || "http://localhost" + api_url;
+var user_api_base_url =  authentication + process.env.USER_API_URL || "http://localhost";
 
 var headers = {
-            'User-Agent': 'request'
-                };
+  'User-Agent': 'request'
+};
+
+ansattListe.cacheAnsattListe();
 
 app.get('/', function(req, res){
   res.sendfile(__dirname + '/web/index.html');
 });
 
+//SOCIALCAST INTEGRATION
 app.get('/messages', function(req, res) {
   request.get({
     url: api_base_url + '/messages',
     json: true,
     headers: headers
-    }, function(error, response, body) {
-      if(error) {
-        console.log("an error has occured. keep calm and carry on.");
-      }
-      res.json(_.first(body, 20));
-    });
+  }, function(error, response, body) {
+    if(error) {
+      console.log("an error has occured. keep calm and carry on.");
+    }
+
+    var messages = _.first(body, 20);
+
+    var messagesWithEmployeeInfo = _.map(messages, function(message) {
+      if(message.user){
+        var employee = getEmployee(message.user.name);
+
+          if(employee){
+            message.user.senioritet = employee.Seniority;
+            message.user.avdeling = employee.Department; 
+          }  
+      } 
+
+      return message;
+
+      });
+
+    res.json(messagesWithEmployeeInfo);
+  });
 });
 
 app.get('/message/:id', function(req, res) {
@@ -36,13 +61,47 @@ app.get('/message/:id', function(req, res) {
     url: api_base_url + '/messages/' + req.params.id,
     json: true,
     headers: headers
-    }, function(error, response, body) {
-      if(error) {
-        console.log("an error has occured. keep calm and carry on.");
-      }
-      res.json(body);
-    });
+  }, function(error, response, body) {
+    if(error) {
+      console.log("an error has occured. keep calm and carry on.");
+    }
+    res.json(body);
+  });
 });
+
+function getEmployee(name) {
+  return _.find(ansattListe.getAnsatte(), function(ansatt) {
+      if(findName(name, ansatt.Name)) {
+        return ansatt;
+      }
+  });
+}
+
+function findName(scName, bekkName) {
+  if(scName == bekkName) {
+    return true;
+  }
+
+  var bekkNames = bekkName.split(" ");
+  var scNames = scName.split(" ");
+  var i = 0;
+
+  _.each(scNames, function(scPartOfName) {
+      _.each(bekkNames, function(bekkPartOfName) {
+          if(scPartOfName == bekkPartOfName) {
+            i++;
+          }
+      });
+  });
+
+  if(i >= 2) {
+    return true;
+  }
+
+  return false;
+}
+
+
 
 
 // if on heroku use heroku port.
